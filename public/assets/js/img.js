eb901b1fe99d5fb04f1e64d569dad8c37f587c65
Creano_Garry <garry@creano.paris>
2022-06-22 14:54:43 +0200

    Delete all unused js files

--- public/assets/js/img.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function()
-{
-    $('img').bind('contextmenu', function(e){
-        return false;
-    }); 
-});
--- public/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$("#btnToggler").on('click', function() {
-    if($("#navToggler").hasClass("collapse")) {
-        // $("#navToggler").addClass("navToggler");
-        $("#navToggler").removeClass("collapse");
-    } else {
-        // $("#navToggler").removeClass("navToggler");
-        $("#navToggler").addClass("collapse");
-    }
-});
